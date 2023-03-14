ae351f37fa143c16d7b5e8044568e3e124011ffb
Philip Kaludercic <philip.kaludercic@fau.de>
2023-03-14 10:10:54 +0100

    Remove auto-generated db/*.c files from repository

--- db/stdlib.c
+++
@@ -1,5 +0,0 @@
-#include "../macs.h"
-#include <stdlib.h>
-DEF(void*, malloc, (size_t size), ( size), (NULL), ENOMEM)
-DEF(void*, calloc, (size_t nmemb, size_t size), ( nmemb, size), (NULL), ENOMEM)
-DEF(void*, realloc, (void *ptr, size_t size), ( ptr, size), (NULL), ENOMEM)
